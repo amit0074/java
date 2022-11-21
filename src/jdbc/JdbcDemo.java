08d638a1c94a732baa86f3b4a60a6b4949c18fbd
amitt <amittalegaonkar5@gmail.com>
2022-11-21 12:05:02 +0530

    JDBC

---
+++ src/jdbc/JdbcDemo.java
@@ -0,0 +1,32 @@
+package jdbc;
+
+import java.sql.*;
+
+public class JdbcDemo {
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/amitdemo","root","Amit@123");
+
+            Statement statement=connection.createStatement();
+
+            String select = "select * from employee";
+
+            ResultSet resultSet = statement.executeQuery(select);
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "
+                        +resultSet.getString(3));
+            }
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
