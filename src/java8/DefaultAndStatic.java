5979cca0efcf8328d8e30ad5e530f7eccd425177
amitt <amittalegaonkar5@gmail.com>
2022-11-30 10:48:56 +0530

    Method Reference

--- src/java8/DefaultAndStatic.java
+++ src/java8/DefaultAndStatic.java
@@ -1,30 +1,29 @@
 package java8;
+interface MyInterface2 {
 
-interface MyInterface2{
     void show();
 
-    default void display(){
-        System.out.println("In Display");
+    default void display() {
+        System.out.println("in display");
     }
+
     static void test(){
-        System.out.println("In test");
+        System.out.println("in test");
     }
 }
 
-
 public class DefaultAndStatic implements MyInterface2 {
+
     public static void main(String[] args) {
 
-        DefaultAndStatic obj=new DefaultAndStatic();
+        DefaultAndStatic obj = new DefaultAndStatic();
         obj.show();
         obj.display();
 
         MyInterface2.test();
-
     }
-
     @Override
     public void show() {
-        System.out.println("In show");
+        System.out.println("in show");
     }
 }
