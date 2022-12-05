d288b7b2c69d81b006bddd1acfca3edad688610c
amitt <amittalegaonkar5@gmail.com>
2022-12-05 10:56:37 +0530

    Optional

---
+++ src/java8/OptionalDemo.java
@@ -0,0 +1,44 @@
+package java8;
+
+import java.util.Optional;
+
+public class OptionalDemo {
+    public static void main(String[] args) {
+
+            Student student = new Student(1, null, "Pune");
+            if (student.name != null) {
+                System.out.println(student.name.toUpperCase());
+            } else {
+                System.out.println("name is null");
+            }
+
+//        Optional<String> s1 = Optional.empty();
+//        System.out.println(s1.get());
+
+//        Optional<String> s2 = Optional.of(student.name);
+//        System.out.println(s2);
+
+            Optional<String> s3 = Optional.ofNullable(student.name);
+            if(s3.isPresent()){
+                System.out.println(s3.get());
+
+                Optional<String> s4=Optional.ofNullable(student.name);
+                System.out.println(s4.orElseGet(()->"Student is Null"));
+            }
+
+        }
+    }
+
+    class Student {
+
+        int id;
+        String name;
+        String address;
+
+        public Student(int id, String name, String address) {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+        }
+    }
+
